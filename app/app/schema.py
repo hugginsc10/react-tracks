0f9dd85e681ae172c952a5de6201a911fcb19fbd
Chas Huggins <hugginsc10@gmail.com>
2020-09-16 13:58:00 -0400

    added create tracks mutation

--- app/app/schema.py
+++ app/app/schema.py
@@ -2,7 +2,10 @@ import graphene
 
 import tracks.schema
 
-class Query(tracks.schema.Query, graphene.ObjectTpe):
+class Query(tracks.schema.Query, graphene.ObjectType):
+  pass
+
+class Mutation(tracks.schema.Mutation, graphene.ObjectType):\
   pass
 
 schema = graphene.Schema(query=Query)
