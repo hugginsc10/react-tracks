f1256571eab3720f4d1258dea5b6f97f655faf16
Chas Huggins <hugginsc10@gmail.com>
2020-09-17 17:03:47 -0400

    added create like mutation

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Track
+from .models import Track, Like
+from users.schema import UserType
 
 
 class TrackType(DjangoObjectType):
@@ -67,9 +68,30 @@ class UpdateTrack(graphene.Mutation):
     
     track.save()
     return UpdateTrack(track=track)
+
+class CreateLike(graphene.Mutation):
+  user = graphene.Field(UserType)
+  track = graphene.Field(TrackType)
   
+  class Arguments:
+    track_id = graphene.Int(required=True)
     
+  def mutate(self, info, track_id):
+      user = info.context.user
+      if user.is_anonymous:
+        raise Exception('Longin to like tracks.')
+      
+      track = Track.objects.get(id=track_id)
+      if not track:
+        raise Exception('Unable to find Track')
+      
+      Like.objects.create(
+        user=user,
+        track=track
+      )
+      return CreateLike(user=user, track=track)
 class Mutation(graphene.ObjectType):
   create_track = CreateTrack.Field()
   update_track = UpdateTrack.Field()
   delete_track = DeleteTrack.Field()
+  create_like = CreateLike.Field()
