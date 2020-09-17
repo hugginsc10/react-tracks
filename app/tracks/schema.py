4bb850ce229b80709f8af3f83f03497b032bd276
Chas Huggins <hugginsc10@gmail.com>
2020-09-17 04:35:38 -0400

    added delete track mutation

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -28,6 +28,22 @@ class CreateTrack(graphene.Mutation):
     track = Track(title=title, description=description, url=url, posted_by=user)
     track.save()
     return CreateTrack(track=track)
+class DeleteTrack(graphene.Mutation):
+  track_id = graphene.Int()
+  
+  class Arguments:
+    track_id = graphene.Int(required=True)
+    
+  def mutate(self, info, track_id):
+    user = info.context.user
+    track = Track.objects.get(id=track_id)
+      
+    if track.posted_by != user:
+      raise Exception('Not permitted to delte this track')
+    
+    track.delete()
+      
+    return DeleteTrack(track_id=track_id)
   
 class UpdateTrack(graphene.Mutation):
   track = graphene.Field(TrackType)
@@ -55,4 +71,5 @@ class UpdateTrack(graphene.Mutation):
     
 class Mutation(graphene.ObjectType):
   create_track = CreateTrack.Field()
-  update_trac = UpdateTrack.Field()
+  update_track = UpdateTrack.Field()
+  delete_track = DeleteTrack.Field()
