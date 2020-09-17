ef03877531f318d00fde459a5777f4e298d59821
Chas Huggins <hugginsc10@gmail.com>
2020-09-17 04:03:56 -0400

    add posted_by

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
+csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
