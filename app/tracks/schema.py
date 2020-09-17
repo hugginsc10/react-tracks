c4b0be84914a9a78b114045e1266ea644a98e2e0
Chas Huggins <hugginsc10@gmail.com>
2020-09-17 17:43:18 -0400

    added Q search for more functionality

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -1,6 +1,8 @@
 import graphene
 from graphene_django import DjangoObjectType
 from graphql import GraphQLError
+from django.db.models import Q
+
 from .models import Track, Like
 from users.schema import UserType
 
@@ -19,7 +21,13 @@ class Query(graphene.ObjectType):
   
   def resolve_tracks(self, info, search=None):
     if search:
-      return Track.objects.filter(title__icontains=search)
+      filter = (
+        Q(title__icontains=search) |
+        Q(description__icontains=search) |
+        Q(url__icontains=search) |
+        Q(posted_by__username__icontains=search)
+      )
+      return Track.objects.filter(filter)
     return Track.objects.all()
   
   def resolve_likes(self, info):
