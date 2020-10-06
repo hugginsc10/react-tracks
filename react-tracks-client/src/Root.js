941324dcea878e46cbc35fc509aada9a5c0dbc45
Chas Huggins <hugginsc10@gmail.com>
2020-10-06 01:39:14 -0400

    finished header component

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -1,18 +1,33 @@
 import React from "react";
-import withRoot from "./withRoot";
 import { Query } from 'react-apollo';
 import { gql } from 'apollo-boost';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import withRoot from "./withRoot";
+import App from "./pages/App";
+import Profile from "./pages/Profile";
+import Header from "./components/Shared/Header"
 
 const Root = () => (
   <Query query={GET_TRACKS_QUERY}>
     {({data, loading, error }) => {
       if (loading) return <div>Loading</div>
       if (error) return <div>console.error();</div>
-      
-      return <div>{JSON.stringify(data)}</div>
+      const currentUser = data.me
+      return (
+        <Router>
+          <>
+            <Header curentUser={currentUser}/>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/profile/:id" component={Profile} />
+            </Switch>
+          </>
+        </Router>
+      )
     }}
   </Query>
 )
+
 const GET_TRACKS_QUERY = gql`
 {
   tracks {
