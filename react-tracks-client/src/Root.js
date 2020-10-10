aac05d369d361a16777da2ca6a92019a6c96a64c
chas <hugginsc10@gmail.com>
2020-10-09 23:09:57 -0400

    fixed header and root.js

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -9,36 +9,47 @@ import Header from "./components/Shared/Header"
 import Loading from './components/Shared/Loading'
 import Error from './components/Shared/Error'
 
+export const UserContext = React.createContext();
 
 const Root = () => (
-  <Query query={GET_TRACKS_QUERY}>
+  <Query query={ME_QUERY}>
     {({data, loading, error }) => {
       if (loading) return <Loading />
       if (error) return <Error error={error} />
       const currentUser = data.me
       return (
         <Router>
-          <>
-            <Header curentUser={currentUser}/>
-          <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/profile/:id" component={Profile} />
+          <UserContext.Provider value={currentUser}>
+            <Header currentUser={currentUser}/>
+            <Switch>
+              <Route exact path="/" component={App} />
+              <Route path="/profile/:id" component={Profile} />
             </Switch>
-          </>
+          </UserContext.Provider>
         </Router>
       )
     }}
   </Query>
 )
 
-const GET_TRACKS_QUERY = gql`
-{
-  tracks {
-    id
-    title
-    description
-    url
-  }
-}
-`
+// const GET_TRACKS_QUERY = gql`
+// {
+//   tracks {
+//     id
+//     title
+//     description
+//     url
+//   }
+// }
+// `
+export const ME_QUERY = gql`
+  {
+    me {
+      id
+      username
+      email
+      }
+    }
+`;
+
 export default withRoot(Root);
