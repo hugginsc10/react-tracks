2c701adb6d80bf0b82a07c85e3bf6dfdcc3b5916
chas <hugginsc10@gmail.com>
2020-11-12 18:37:50 -0500

    changed database info in settings.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 react-trax-backend/manage.py runserver 0.0.0.0:$PORT
+web: gunicorn react-trax-backend.wsgi
--- react-trax-backend/app/settings.py
+++ react-trax-backend/app/settings.py
@@ -98,26 +98,26 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'trax',
-#         'USER': 'chas',
-#         'PASSWORD': 'trax',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'deqfkkclfv6ii4',
-        'USER': '',
-        'PASSWORD': 'cudnnutsbmyalm',
-        'HOST': 'ec2-54-146-142-58.compute-1.amazonaws.com',
+        'NAME': 'trax',
+        'USER': 'chas',
+        'PASSWORD': 'trax',
+        'HOST': 'localhost',
         'PORT': '5432'
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'deqfkkclfv6ii4',
+#         'USER': '',
+#         'PASSWORD': 'cudnnutsbmyalm',
+#         'HOST': 'ec2-54-146-142-58.compute-1.amazonaws.com',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # Password validation
