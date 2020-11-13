706382e9fdd4de153df563f724233d2063ad9bc4
chas <hugginsc10@gmail.com>
2020-11-12 21:20:39 -0500

    changes

--- tracks-backend/app/wsgi.py
+++ tracks-backend/app/wsgi.py
@@ -7,11 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
+import os   
 
 from django.core.wsgi import get_wsgi_application
 # from whitenoise.django import DjangoWhiteNoise
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tracks-backend.app.settings')
 
 application = get_wsgi_application()
 # application = DjangoWhiteNoise(application)
